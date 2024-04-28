6c7f344c58450f1956f66d615fb2f50b0f8490ba
saidechlahbi <echlahbisaid@gmail.com>
2024-04-28 13:41:12 +0000

    gg

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import Base
+from models.review import Review
 from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
