ee28235184d18afb4a7984883ca8a00c7aceb8c6
Joe-0009 <youssefrachidi1994@gmail.com>
2024-04-29 09:31:37 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 Contains the FileStorage class
 """
 
+import shlex
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -25,14 +26,21 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
-        if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
-        return self.__objects
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
